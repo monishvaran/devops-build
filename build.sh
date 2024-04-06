a4b4d2d12b804cc665733be40894677a53e76457
monishvaran <153340778+monishvaran@users.noreply.github.com>
2024-04-06 17:10:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@
 #login into DockerHub:
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
 
-#stopping existing container:
-docker stop react
-docker rm react
+
 
 #building a image:
 docker build -t devopstest:react-application .
 
-#running a container from the created image:
-docker run -d -it --name react -p 80:80 devopstest:react-application
+docker-compose down || true
+
+docker-compose up -d
