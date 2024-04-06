5f968bcd23e886ed950b4ed0b7a0590128dd6fbb
monishvaran <153340778+monishvaran@users.noreply.github.com>
2024-04-06 17:01:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Define variables
-IMAGE_NAME="devopstest"
-TAG="react-application"
+#login into DockerHub:
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
 
-# Build Docker image
-docker build -t "$IMAGE_NAME:$TAG" .
+#stopping existing container:
+docker stop react
+docker rm react
 
+#building a image:
+docker build -t devopstest:react-application .
+
+#running a container from the created image:
+docker run -d -it --name react -p 80:80 devopstest:react-application
